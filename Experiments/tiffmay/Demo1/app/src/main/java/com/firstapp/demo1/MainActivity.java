d040ff584a133f6c3f59ca29c1481a9ba9bd98d7
Tiffany <tiff.mayberry@gmail.com>
2021-02-05 17:07:18 -0600

    #1 add button to show msg and button to go to another screen

--- Experiments/tiffmay/Demo1/app/src/main/java/com/firstapp/demo1/MainActivity.java
+++ Experiments/tiffmay/Demo1/app/src/main/java/com/firstapp/demo1/MainActivity.java
@@ -2,13 +2,33 @@ package com.firstapp.demo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.buttonMessage);
+        b2 = findViewById(R.id.buttonNext);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello! This is a test. A simple test for DEMO 1.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b2.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(i);
+        });
     }
 }
