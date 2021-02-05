d6994e146f29d5b225e1c6f9fe5cf64afefffd31
david <david@DavidKali.homebase.com>
2021-02-05 15:29:41 -0600

    #2  Added a fun post method and some get stuff

---
+++ Experiments/djwolfe/exp1/helloworld/src/main/java/coms309/Apple.java
@@ -0,0 +1,17 @@
+package coms309;
+
+class Apple {
+	private String color;
+	private int size;
+
+	public Apple(String color, int size){
+		this.color = color;
+		this.size = size;
+	}
+	public String getColor(){
+		return this.color;
+	}
+	public int getSize(){
+		return this.size;
+	}
+}
--- Experiments/djwolfe/exp1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/djwolfe/exp1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -1,8 +1,14 @@
 package coms309;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+import coms309.Apple;
 
 @RestController
 class WelcomeController {
@@ -15,5 +21,13 @@ class WelcomeController {
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
+    }
+    @GetMapping("/exp1/wow")
+    public String uniWow(HttpServletRequest req){
+	return "~~~UNI-WOW~~~<br> You're accessing this site from: " + req.getRemoteAddr();
+    }
+    @RequestMapping(method=RequestMethod.POST, path="/exp1/post")
+    public String myPost(@RequestBody Apple apple){
+	    return "Apple color: " + apple.getColor() + "<br>Apple size: " + apple.getSize();
     }
 }
