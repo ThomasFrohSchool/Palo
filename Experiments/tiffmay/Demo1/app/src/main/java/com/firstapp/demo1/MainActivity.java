df7ef9e306faf239b19d8e7e77d2cb6c3a2ae036
Tiffany <tiff.mayberry@gmail.com>
2021-02-08 09:13:00 -0600

    test

--- Experiments/tiffmay/Demo1/app/src/main/java/com/firstapp/demo1/MainActivity.java
+++ Experiments/tiffmay/Demo1/app/src/main/java/com/firstapp/demo1/MainActivity.java
@@ -3,13 +3,17 @@ package com.firstapp.demo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    Button b1, b2;
+    Button b1, b2, b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         b1 = findViewById(R.id.buttonMessage);
         b2 = findViewById(R.id.buttonNext);
+        b3 = findViewById(R.id.buttonChangeColor);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +35,16 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(i);
         });
+
+        b3.setOnClickListener(v -> {
+            //Change activities title
+            TextView title = findViewById(R.id.mainTitle);
+            title.setTextColor(this.getRandomColor());
+        });
+    }
+
+    private int getRandomColor(){
+        Random rand = new Random();
+        return Color.argb(255, rand.nextInt(),rand.nextInt(), rand.nextInt());
     }
 }
