32c4bdf56f842a019475166e001950fea9ea7f56
Tiffany <tiff.mayberry@gmail.com>
2021-02-08 09:13:00 -0600

    #2 add changing color title / back button

--- Experiments/tiffmay/Demo1/app/src/main/java/com/firstapp/demo1/HomeActivity.java
+++ Experiments/tiffmay/Demo1/app/src/main/java/com/firstapp/demo1/HomeActivity.java
@@ -2,13 +2,22 @@ package com.firstapp.demo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        back = findViewById(R.id.backButton);
+        back.setOnClickListener(v -> {
+            Intent i = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(i);
+        });
     }
 }
