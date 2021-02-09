810ad45851035bf5fd9442abfc51f1f2df86a9c9
djwolfe <djwolfe@iastate.edu>
2021-02-09 17:19:54 -0600

    added sql import

--- Backend/src/main/java/coms309/endpoints.java
+++ Backend/src/main/java/coms309/endpoints.java
@@ -1,5 +1,6 @@
 package coms309;
 
+import java.sql.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
