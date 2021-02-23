32db9d6c641aa9564775ce4cd6df64a909141706
Thomas Frohwein <tfroh@iastate.edu>
2021-02-23 00:33:14 -0600

    Added JSON request to Volley Example

--- Experiments/tfroh/tfrohVolleyEx/app/src/main/java/com/example/tfrohvolleyex/MainActivity.java
+++ Experiments/tfroh/tfrohVolleyEx/app/src/main/java/com/example/tfrohvolleyex/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button str, json, img;
 
     @Override
@@ -19,14 +19,27 @@ public class MainActivity extends AppCompatActivity {
         json = (Button) findViewById(R.id.jsonReq);
         img = (Button) findViewById(R.id.imgReq);
 
-        str.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, StringActivity.class);
-                startActivity(i);
-            }
-        });
+        str.setOnClickListener(this);
+        json.setOnClickListener(this);
+        img.setOnClickListener(this);
     }
 
-
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.stringReq:
+                startActivity(new Intent(MainActivity.this, StringActivity.class));
+                break;
+                
+            case R.id.jsonReq:
+                startActivity(new Intent(MainActivity.this, JsonActivity.class));
+                break;
+                
+            case R.id.imgReq:
+                startActivity(new Intent(MainActivity.this, ImageActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
 }
