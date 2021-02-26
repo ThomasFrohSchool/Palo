1f6c8e73a8f458cdbab2bc1bd3d9598243345fdb
David Wolfe <djwolfe@iastate.edu>
2021-02-26 15:53:14 -0800

    #11 Created SpotifyController

---
+++ Backend/src/main/java/coms309/Spotify/SpotifyController.java
@@ -0,0 +1,21 @@
+package coms309.Spotify;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SpotifyController {
+
+    private String success = "\"message\":\"success\"";
+    private String failure = "\"message\":\"failure\"";
+
+    @GetMapping(path = "/search")
+    //TODO: Everything
+}
