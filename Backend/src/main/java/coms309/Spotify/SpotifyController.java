4d6e4097d9fb2d3a8ecd0339c7620b599f489769
mboser <mboser@iastate.edu>
2021-03-02 15:11:42 -0600

    #11Added some random auth lines to search

--- Backend/src/main/java/coms309/Spotify/SpotifyController.java
+++ Backend/src/main/java/coms309/Spotify/SpotifyController.java
@@ -1,6 +1,7 @@
 package coms309.Spotify;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -29,33 +30,28 @@ public class SpotifyController {
     private String success = "\"message\":\"success\"";
     private String failure = "\"message\":\"failure\"";
 
-
-    private String getToken(){
-        try{
+    private String getToken() {
+        try {
             String url = "https://accounts.spotify.com/api/token";
             URL obj = new URL(url);
-    
-    
-            Map<String,String> body = new LinkedHashMap<>();
-            body.put("client_id", "b0b9efa4be804ee89bb797a2ea88e0f8");
-            body.put("client_secret", "e2cfbce6769442379e28d04d1bbb3be1");
+
+            Map<String, String> body = new LinkedHashMap<>();
+            body.put("client_id", "b82ef89345fa42a7893a0f199d64439f");
+            body.put("client_secret", "92ca45fcd0ee42239ca5a259c88baf11");
             body.put("grant_type", "client_credentials");
-    
-    
+
             StringBuilder postData = new StringBuilder();
-    
+
             for (Map.Entry bod : body.entrySet()) {
-                if (postData.length() != 0) postData.append('&');
-                postData.append(URLEncoder.encode(String.valueOf(bod.getKey()),"UTF-8"));
+                if (postData.length() != 0)
+                    postData.append('&');
+                postData.append(URLEncoder.encode(String.valueOf(bod.getKey()), "UTF-8"));
                 postData.append('=');
                 postData.append(URLEncoder.encode(String.valueOf(bod.getValue()), "UTF-8"));
             }
 
+            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
 
-    
-            byte[] postDataBytes  = postData.toString().getBytes("UTF-8");
-
-    
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
             con.setRequestMethod("POST");
@@ -65,44 +61,40 @@ public class SpotifyController {
             con.setDoOutput(true);
             con.getOutputStream().write(postDataBytes);
 
-
             int responseCode = con.getResponseCode();
-
 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
             String line;
             StringBuffer response = new StringBuffer();
-            while ((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 response.append(line);
             }
             in.close();
 
-
-    
             System.out.println(response.toString());
-    
-    
+
             JSONObject myResponse = new JSONObject(response.toString());
             System.out.println(myResponse.getString("access_token"));
 
-
-    
             return myResponse.getString("access_token");
-    
-        }
-        catch(Exception e){
+
+        } catch (Exception e) {
             return e.toString();
         }
     }
 
     @GetMapping(path = "/token")
-    public String token(){
+    public String token() {
         return getToken();
     }
 
-
     @GetMapping(path = "/search")
-    public String search(@RequestParam("q") String q){
+    public String search(@RequestParam("q") String q) {
+        String authToken = getToken();
+
+        String url = "https://api.spotify.com/v1/search";
+        
+
         return q;
     }
 }
