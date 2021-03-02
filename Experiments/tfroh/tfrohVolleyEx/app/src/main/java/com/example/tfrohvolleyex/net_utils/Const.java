38539b9fd6996cfb99d1da1a6aae821cb508624d
Thomas Frohwein <tfroh@iastate.edu>
2021-03-01 18:21:37 -0600

    Changed url

--- Experiments/tfroh/tfrohVolleyEx/app/src/main/java/com/example/tfrohvolleyex/net_utils/Const.java
+++ Experiments/tfroh/tfrohVolleyEx/app/src/main/java/com/example/tfrohvolleyex/net_utils/Const.java
@@ -4,7 +4,8 @@ public class Const {
     //public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
     public static final String URL_JSON_OBJECT = "https://7ed5580a-d41f-4263-8e30-070bfb821933.mock.pstmn.io/api/userdata/10";
     //public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
-    public static final String URL_JSON_ARRAY = "https://7ed5580a-d41f-4263-8e30-070bfb821933.mock.pstmn.io/api/userdata/11";
+    //public static final String URL_JSON_ARRAY = "https://7ed5580a-d41f-4263-8e30-070bfb821933.mock.pstmn.io/api/userdata/11";
+    public static final String URL_JSON_ARRAY = "https://ddf54325-6e13-4455-91a5-91a3eacea835.mock.pstmn.io/api/userdata/11";
     public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
     public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
 }
