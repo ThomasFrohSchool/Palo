a8f1a09ef94999599e757dc5e17fc20f028b3b2f
Thomas Frohwein <tfroh@iastate.edu>
2021-03-01 18:08:17 -0600

    Added Postman example

--- Experiments/tfroh/tfrohVolleyEx/app/src/main/java/com/example/tfrohvolleyex/net_utils/Const.java
+++ Experiments/tfroh/tfrohVolleyEx/app/src/main/java/com/example/tfrohvolleyex/net_utils/Const.java
@@ -1,8 +1,10 @@
 package com.example.tfrohvolleyex.net_utils;
 
 public class Const {
-    public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
-    public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+    //public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
+    public static final String URL_JSON_OBJECT = "https://7ed5580a-d41f-4263-8e30-070bfb821933.mock.pstmn.io/api/userdata/10";
+    //public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+    public static final String URL_JSON_ARRAY = "https://7ed5580a-d41f-4263-8e30-070bfb821933.mock.pstmn.io/api/userdata/11";
     public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
     public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
 }
