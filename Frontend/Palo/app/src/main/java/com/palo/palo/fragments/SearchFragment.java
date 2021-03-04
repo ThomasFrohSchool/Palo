9c0c1ecae20c3e0ea49c96f988bf49beb59b89a0
Thomas Frohwein <tfroh@iastate.edu>
2021-03-04 01:23:36 -0600

    #11 Added string function

--- Frontend/Palo/app/src/main/java/com/palo/palo/fragments/SearchFragment.java
+++ Frontend/Palo/app/src/main/java/com/palo/palo/fragments/SearchFragment.java
@@ -16,8 +16,11 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.palo.palo.R;
 import com.palo.palo.volley.VolleySingleton;
 
@@ -26,10 +29,12 @@ public class SearchFragment extends Fragment {
     private EditText song;
     private TextView req;
     private Button b;
-    String url = "https://60dfb9a0-6de1-47ff-9b9b-69c16d317496.mock.pstmn.io";
+    private String url = "https://60dfb9a0-6de1-47ff-9b9b-69c16d317496.mock.pstmn.io";
     private ProgressDialog p;
-    private String TAG = SearchFragment.class.getSimpleName();
+    private String JSONTAG = SearchFragment.class.getSimpleName();
+    private String STRINGTAG = SearchFragment.class.getSimpleName();
     private String tag_json_obj = "jobj_req", tag_json_array = "jarray_req";
+    private String tag_string_req = "string_req";
 
     public SearchFragment() {}
 
@@ -50,24 +55,36 @@ public class SearchFragment extends Fragment {
         req = view.findViewById(R.id.songResult);
         b = view.findViewById(R.id.button);
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                makeJsonSongRequest();
-            }
-        });
+        p = new ProgressDialog(getActivity().getApplicationContext());
+        p.setMessage("Loading...");
+        p.setCancelable(false);
+
+        b.setOnClickListener(v -> makeStringSongRequest());
     }
 
     private void makeJsonSongRequest() {
+        //p.show();
         JsonObjectRequest jsonSongRequest = new JsonObjectRequest(Request.Method.GET,
                 url + "/search?q=" + song.getText().toString(), null,
                 response -> {
-                    Log.d(TAG, response.toString());
+                    Log.d(JSONTAG, response.toString());
                     req.setText("Response: " + response.toString());
-                }, error -> VolleyLog.d(TAG, "Error: " + error.getMessage()));
+                }, error -> VolleyLog.d(JSONTAG, "Error: " + error.getMessage()));
+        //p.hide();
 
         VolleySingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(jsonSongRequest);
     }
 
+    private void makeStringSongRequest() {
+        //p.show();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                url + "/search?q=" + song.getText().toString(),
+                response -> {
+                    Log.d(STRINGTAG, response.toString());
+                    req.setText("Response: " + response.toString());
+                }, error -> VolleyLog.d(STRINGTAG, "Error: " + error.getMessage()));
+        //p.hide();
 
+        VolleySingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(stringRequest);
+    }
 }
