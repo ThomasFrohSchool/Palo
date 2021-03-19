a3319e12a5025e8665030e67ffb154e831651d84
David Wolfe <djwolfe@iastate.edu>
2021-03-19 15:25:09 -0500

    #14 Initial dependencies added

--- Backend/src/test/java/JUnitControllerTest.java
+++ Backend/src/test/java/JUnitControllerTest.java
@@ -5,6 +5,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;  
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
 @SpringBootTest  
 public class JUnitControllerTest {
 
