5abb2df03f2207b97c8ade5f5ccfab05e338d2b6
Thomas Frohwein <tfroh@iastate.edu>
2021-03-22 18:27:56 -0500

    #6 Created a working view for the users' own posts in the profile page

--- Frontend/Palo/app/src/main/java/com/palo/palo/fragments/ProfileFragment.java
+++ Frontend/Palo/app/src/main/java/com/palo/palo/fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.palo.palo.fragments;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,31 +11,44 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.palo.palo.R;
 import com.palo.palo.SharedPrefManager;
 import com.palo.palo.model.User;
 import com.palo.palo.volley.VolleySingleton;
+import com.palo.palo.FeedAdapter;
+import com.palo.palo.model.Palo;
+import com.palo.palo.model.Song;
+import com.palo.palo.model.User;
+import com.palo.palo.volley.VolleySingleton;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
     //temporary url
-    private String url = "https://440b43ef-556f-4d7d-a95d-081ca321b8f9.mock.pstmn.io";
+    private static String url = "https://440b43ef-556f-4d7d-a95d-081ca321b8f9.mock.pstmn.io";
     private TextView profileName;
     private ImageView profileImage;
     private TextView paloAmt;
-    private TextView palos;
+    //private TextView palos;
     private TextView followerAmt;
-    private TextView followers;
+    //private TextView followers;
     private TextView followingAmt;
-    private TextView following;
-    private User user;
+    //private TextView following;
+    private static User user;
     private ProgressDialog p;
+    private RecyclerView r;
 
     public ProfileFragment() {}
 
@@ -56,11 +70,12 @@ public class ProfileFragment extends Fragment {
         profileImage = view.findViewById(R.id.profileImage);
         profileName = view.findViewById(R.id.profileName);
         paloAmt = view.findViewById(R.id.paloAmt);
-        palos = view.findViewById(R.id.Palos);
+        //palos = view.findViewById(R.id.Palos);
         followerAmt = view.findViewById(R.id.followerAmt);
-        followers = view.findViewById(R.id.Followers);
+        //followers = view.findViewById(R.id.Followers);
         followingAmt = view.findViewById(R.id.followingAmt);
-        following = view.findViewById(R.id.Following);
+        //following = view.findViewById(R.id.Following);
+        r = view.findViewById(R.id.userPalos);
         //p = new ProgressDialog(getActivity().getApplicationContext());
         //p.setMessage("Loading...");
 
@@ -73,6 +88,8 @@ public class ProfileFragment extends Fragment {
 
         profileName.setText(user.getUsername());
         getProfile();
+        //getUserPalos();
+        extractPalos();
     }
 
     private void getProfile() {
@@ -90,10 +107,56 @@ public class ProfileFragment extends Fragment {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                }, error -> {
-                    error.printStackTrace();
-                });
+                }, error -> error.printStackTrace());
         //p.hide();
-        VolleySingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(j);
+        //VolleySingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(j);
+    }
+
+    private void extractPalos() {
+        JsonArrayRequest request = getUserPalos(r, getActivity().getApplicationContext());
+        VolleySingleton.getInstance(getActivity().getApplicationContext()).addToRequestQueue(request);
+    }
+
+    private static JsonArrayRequest getUserPalos(RecyclerView recyclerView, Context context) {
+        return new JsonArrayRequest(Request.Method.GET, url + "/Palo?q=" + user.getUsername(), null,
+                response -> {
+                    List<Palo> palos = new ArrayList<>();
+                    for(int i = 0; i < response.length(); i++) {
+                        try {
+                            palos.add(extractPalo(response.getJSONObject(i)));
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                    recyclerView.setAdapter(new FeedAdapter(context, palos));
+                }, error -> error.printStackTrace());
+    }
+
+    private static Palo extractPalo(JSONObject paloJSON) throws JSONException {
+        Palo palo = new Palo();
+        palo.setAuthor(extractUser(paloJSON.getJSONObject("author")));
+        palo.setPostDate(paloJSON.getString("postdate"));
+        palo.setCaption(paloJSON.getString("caption"));
+        palo.setAttachedSong(extractSong(paloJSON.getJSONObject("song")));
+        return palo;
+    }
+
+    // Example --> {"song": "Freakin' Out On the Interstate", "artist":"Briston Maroney","album_cover": "https://i0.wp.com/altangeles.com/wp-content/uploads/2018/12/briston.jpg?fit=640%2C640&ssl=1"}
+    private static Song extractSong(JSONObject songJSON) throws JSONException {
+        Song song = new Song();
+        song.setTitle(songJSON.getString("song"));
+        song.setArtist(songJSON.getString("artist"));
+        song.setAlbumCover(songJSON.getString("album_cover"));
+        return song;
+    }
+
+    // Example --> {"username": "tiffmay", "profile_image": "fillimage link here"},
+    private static User extractUser(JSONObject userJSON) throws JSONException{
+        User user = new User();
+        user.setUsername(userJSON.getString("username"));
+        user.setProfileImage(userJSON.getString("profile_image"));
+        return user;
     }
 }
